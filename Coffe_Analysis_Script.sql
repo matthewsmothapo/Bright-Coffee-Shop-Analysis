bb71ab1bfa37bcee93d647ee950c75f1a0bae414
Matthews Mothapo <mothapomatthews0@gmail.com>
2025-08-31 23:04:57 +0200

    Update Coffe_Analysis_Script.sql

--- Coffe_Analysis_Script.sql
+++ Coffe_Analysis_Script.sql
@@ -1,3 +1,5 @@
+
+-- Preview the first 10 rows from the Coffe table to validate the structure and content
 SELECT
   *
 FROM
@@ -5,17 +7,21 @@ FROM
 LIMIT
   10;
 
+-- Extract date components and classify transactions by day and time
+
   SELECT TO_DATE(transaction_date) AS purchase_date,
          MONTHNAME(TO_DATE(transaction_date)) AS name_of_month,
          DAYOFMONTH(TO_DATE(transaction_date)) AS day_of_month,
          TO_CHAR(TO_DATE(transaction_date), 'YYYYMM') AS month_id,
          DAYNAME(TO_DATE(transaction_date)) AS day_name,
 
+   -- Classifying day as a Weekday or Weekend
          CASE
          WHEN day_name NOT IN ('Sat','Sun') THEN 'Weekday'
          Else 'Weekend'
          END AS day_classification,
 
+   -- Classifying time into business intervals
          CASE
           WHEN transaction_time BETWEEN '06:00:00' AND '08:59:59' THEN 'Early Morning: 6am-9am'
           WHEN transaction_time BETWEEN '09:00:00' AND '11:59:59' THEN 'Morning: 09am-12pm'
@@ -25,18 +31,27 @@ LIMIT
           END AS time_classification,
           HOUR(transaction_time) hour_of_day,
 
+   -- Aggregate metrics
+   
         COUNT(DISTINCT transaction_id) AS number_of_sales,
         COUNT (DISTINCT store_id) AS Number_of_stores,
         COUNT (DISTINCT product_id) AS Number_of_different_products,
+
+   -- Categorical dimensions
+   
         product_category,
         product_type,
         product_detail,
         store_location,
-        ROUND(SUM(IFNULL(transaction_qty,0)*IFNULL(unit_price,0)),0) AS total_revenue,
-  FROM
+       
+   -- Revenue calculation
+         ROUND(SUM(IFNULL(transaction_qty,0)*IFNULL(unit_price,0)),0) AS total_revenue,
+   
+   FROM
   "BRIGHTCOFFESALES"."PUBLIC"."COFFE"
   GROUP BY ALL;
 
+-- High level summary of sales, store coverage, and product diversity
   SELECT
         COUNT (DISTINCT transaction_id) AS number_of_sales,
         COUNT (DISTINCT store_id) AS Number_of_stores,
@@ -46,7 +61,8 @@ LIMIT
   "BRIGHTCOFFESALES"."PUBLIC"."COFFE"
   GROUP BY ALL;
 
-  
+  -- Identifying earliest and latest transaction times for business hours
+
  SELECT MIN(transaction_time) AS opening_time,
         MAX(transaction_time) AS closing_time
 FROM
